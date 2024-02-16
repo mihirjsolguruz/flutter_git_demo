ed625c8433165466f61ea22d50db6f79405b37a1
mihirj <mhrjsh1@gmail.com>
2024-02-16 12:35:21 +0530

    feat: adding black color

--- lib/color.dart
+++ lib/color.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color redAccent = Colors.redAccent.shade200;
 Color yellowAccent = Colors.redAccent.shade200;
 Color orangeAccent = Colors.redAccent.shade200;
+Color blackAccent = Colors.black;
