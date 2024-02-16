3c81548028c96a897fe72b4aa4b0c94053618bdd
mihirj <mhrjsh1@gmail.com>
2024-02-16 12:24:37 +0530

    feat: adding yellow and orange accent color

--- lib/color.dart
+++ lib/color.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color redAccent = Colors.redAccent.shade200;
+Color yellowAccent = Colors.redAccent.shade200;
+Color orangeAccent = Colors.redAccent.shade200;
